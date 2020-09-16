e966c2f35cd4747f3286ff4e0ef674c992755203
Alican <redundantadam@gmail.com>
2020-09-16 22:36:39 +0300

    second commit

--- src/carMain.java
+++ src/carMain.java
@@ -4,7 +4,7 @@ public class carMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("upgraded");
 		carTest myCar = new carTest("BMW","Black",145000,2020);
 		System.out.println("*********************************");
 		carTest fatherCar = new carTest("Mercedes","White",145000,2020);
